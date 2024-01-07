c9b510f35b3630a6aaddabb749a3264b96ef8a1b
wladi <wladimir2000@protonmail.com>
2024-01-06 21:47:19 -0300

    Try to connect to MongoDB Atlas

--- api/app/routers/authentication.py
+++ api/app/routers/authentication.py
@@ -15,56 +15,55 @@ class UserRegistration(BaseModel):
     username: str
     password: str
 
-# Configurar las credenciales de autenticación de la BDD
-username = "admin"
-password = "myPassword123"
 
-# Crear una instancia del cliente de MongoDB
-# mongo_client = MongoClient("mongodb://coleccionista-bd-test:27017/",
-#                            username=username,
-#                            password=password)
-#
-# # Obtener una referencia a la base de datos
-# mongo_db = mongo_client["coleccionista-bd-test"]
-#
-# usuarios_collection = mongo_db["usuarios"]
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
 if os.environ.get("PRODUCTION") == "True":
-	@router.get("/is/prod")
-	async def is_prod():
-		return os.environ.get("PRODUCTION")
+	password = os.environ.get("MONGO_DB_ATLAS_PASSWORD")
+	mongo_db_atlas_uri = f"mongodb+srv://wlurzuaProfesionalPortafolio:{password}@cluster0.oaoeslc.mongodb.net/?retryWrites=true&w=majority"
+	mongo_client = MongoClient(mongo_db_atlas_uri)
+	mongo_db = mongo_client["Cluster0"]
 else:
-	@router.get("/is/dev")
-	async def is_dev():
-		return os.environ.get("PRODUCTION")
+	# Configurar las credenciales de autenticación de la BDD
+	username = "admin"
+	password = "myPassword123"
+
+	#Crear una instancia del cliente de MongoDB
+	mongo_client = MongoClient("mongodb://coleccionista-bd-test:27017/",
+							   username=username,
+							   password=password)
+
+	# Obtener una referencia a la base de datos
+	mongo_db = mongo_client["coleccionista-bd-test"]
+
+usuarios_collection = mongo_db["usuarios"]
 
 @router.get("/get/hello")
 async def get_hello():
 	return "Hello world!"
 
-# @router.get("/get/{setting}")
-# async def get_user(setting: str):
-#     if setting == "all":
-#         return json.loads(dumps(usuarios_collection.find()))
-#     else:
-#         usuario = usuarios_collection.find_one({"username": setting})
-#         return json.loads(dumps(usuario))
-#
-# @router.post("/register/", status_code=201)
-# async def create_user(user: UserRegistration):
-#     if usuarios_collection.find_one({"username": user.username}):
-#         return False
-#     # Encriptar la contraseña
-#     hashed_password = bcrypt.hashpw(
-#         user.password.encode('utf-8'), bcrypt.gensalt())
-#
-#     usuarios_collection.insert_one(
-#         {"username": user.username, "password": hashed_password.decode('utf-8')})
-#
-#     usuario_check = usuarios_collection.find_one({"username": user.username})
-#     if usuario_check:
-#         return str(usuario_check["_id"])
-#     else:
-#         return False
+@router.get("/get/{setting}")
+async def get_user(setting: str):
+    if setting == "all":
+        return json.loads(dumps(usuarios_collection.find()))
+    else:
+        usuario = usuarios_collection.find_one({"username": setting})
+        return json.loads(dumps(usuario))
+
+@router.post("/register/", status_code=201)
+async def create_user(user: UserRegistration):
+    if usuarios_collection.find_one({"username": user.username}):
+        return False
+    # Encriptar la contraseña
+    hashed_password = bcrypt.hashpw(
+        user.password.encode('utf-8'), bcrypt.gensalt())
+
+    usuarios_collection.insert_one(
+        {"username": user.username, "password": hashed_password.decode('utf-8')})
+
+    usuario_check = usuarios_collection.find_one({"username": user.username})
+    if usuario_check:
+        return str(usuario_check["_id"])
+    else:
+        return False
