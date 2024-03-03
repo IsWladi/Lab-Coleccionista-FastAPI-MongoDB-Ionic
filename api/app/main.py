05d79cc0cd000aabdef5b07a8afbd489ffa4e18d
IsWladi <isaacx3984@outlook.com>
2024-03-03 16:44:32 -0300

    fix(fastapi): fix cors, allow all origins

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from .routers import examples, auth
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=["http://localhost:8100"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 #include routers
 app.include_router(auth.router)
