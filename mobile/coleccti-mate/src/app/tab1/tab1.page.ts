35378af3bcdf77325b68a8e4025d68c18a336149
IsWladi <isaacx3984@outlook.com>
2024-03-03 16:52:33 -0300

    feat(ionic): implement basic usage of the service

--- mobile/coleccti-mate/src/app/tab1/tab1.page.ts
+++ mobile/coleccti-mate/src/app/tab1/tab1.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExamplesService } from '../api/examples.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  constructor(private apiService: ExamplesService) {
+  }
 
-  constructor() {}
-
+  ngOnInit() {
+    this.apiService.getAllUsersWithoutAuth();
+  }
+  //
 }
