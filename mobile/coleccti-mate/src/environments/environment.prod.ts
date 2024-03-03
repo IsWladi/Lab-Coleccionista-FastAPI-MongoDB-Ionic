47c22285a74f76cd14778418ccae3e1e8f4c5ad8
IsWladi <isaacx3984@outlook.com>
2024-03-03 16:46:07 -0300

    feat(ionic): add api url for prod or local

--- mobile/coleccti-mate/src/environments/environment.prod.ts
+++ mobile/coleccti-mate/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://lab_portafolio-1-k1767315.deta.app/', // use ionic build --prod to build for production
+
 };
